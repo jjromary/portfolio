df845c41927326f6e57055d769ac8281b603d3d1
José Romary Brandão <joseromarybrandao@gmail.com>
2023-03-24 02:32:22 -0300

    update: card projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import { HomeContainer, SkillImgs, SkillList, SkillsContainer, SkillsContent, SkillStatus } from "../styles/pages/home";
+import { HomeContainer, Project, ProjectContent, ProjectDescription, ProjectLink, ProjectsContainer, ProjectsList, SkillImgs, SkillList, SkillsContainer, SkillsContent, SkillStatus } from "../styles/pages/home";
 
-// import projeto1 from '../assets/01.jpg';
-// import projeto2 from '../assets/02.jpg';
-// import projeto3 from '../assets/03.jpg';
-// import projeto4 from '../assets/04.jpg';
 
+import projeto1 from '../assets/01.jpg';
+
+import { CaretRight } from "phosphor-react";
 import css from '../assets/css.png';
 import git from '../assets/git.png';
 import html from '../assets/html.png';
@@ -28,73 +27,49 @@ export default function Home() {
               Tenho Experiência
             </SkillStatus>
             <SkillImgs>
-              <Image src={html} alt='projeto' width={60} height={60} />
-              <Image src={css} alt='projeto' width={60} height={60} />
-              <Image src={js} alt='projeto' width={60} height={60} />
-              <Image src={react} alt='projeto' width={60} height={60} />
-              <Image src={ts} alt='projeto' width={60} height={60} />
-              <Image src={git} alt='projeto' width={60} height={60} />
+              <Image src={html} alt='projeto' width={50} height={50} />
+              <Image src={css} alt='projeto' width={50} height={50} />
+              <Image src={js} alt='projeto' width={50} height={50} />
+              <Image src={react} alt='projeto' width={50} height={50} />
+              <Image src={ts} alt='projeto' width={50} height={50} />
+              <Image src={git} alt='projeto' width={50} height={50} />
             </SkillImgs>
             <SkillStatus>
               Estou Estudando
             </SkillStatus>
             <SkillImgs>
-              <Image src={next} alt='projeto' width={60} height={60} />
+              <Image src={next} alt='projeto' width={50} height={50} />
             </SkillImgs>
           </SkillList>
         </SkillsContent>
       </SkillsContainer>
 
-      {/* <ProjectContent>
-        <Project >
-          <Image src={projeto1} alt='projeto' width={600} height={480} />
+      <ProjectsContainer>
+        <ProjectContent>
+          <span>Projetos</span>
+          <ProjectsList>
+            <Project >
+              <Image src={projeto1} alt='projeto' width={600} height={200} />
 
-          <footer>
-            <strong>Disposable E-mail</strong>
-            <span>
-              Descrição: Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1520s, when an
-              unknown printer took a galley of type and scrambled it to make a type specimen book
-            </span>
-          </footer>
-        </Project>
-        <Project >
-          <Image src={projeto2} alt='projeto' width={600} height={480} />
+              <ProjectDescription>
+                <strong>Disposable E-mail</strong>
+                <span>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1520s, when an.
+                </span>
 
-          <footer>
-            <strong>Disposable E-mail</strong>
-            <span>
-              Descrição: Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1520s, when an
-              unknown printer took a galley of type and scrambled it to make a type specimen book
-            </span>
-          </footer>
-        </Project>
-        <Project>
-          <Image src={projeto3} alt='projeto' width={600} height={480} />
+                <ProjectLink>
+                  Acessar <CaretRight />
+                </ProjectLink>
+              </ProjectDescription>
 
-          <footer>
-            <strong>Disposable E-mail</strong>
-            <span>
-              Descrição: Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-              unknown printer took a galley of type and scrambled it to make a type specimen book
-            </span>
-          </footer>
-        </Project>
-        <Project >
-          <Image src={projeto4} alt='projeto' width={600} height={480} />
+            </Project>
 
-          <footer>
-            <strong>Disposable E-mail</strong>
-            <span>
-              Descrição: Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-              unknown printer took a galley of type and scrambled it to make a type specimen book
-            </span>
-          </footer>
-        </Project>
-      </ProjectContent> */}
+
+          </ProjectsList>
+        </ProjectContent>
+      </ProjectsContainer>
+
 
 
     </HomeContainer>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,88 +1,121 @@
 import { styled } from "..";
 
-export const HomeContainer = styled('main', {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center', 
-  margin: '0 auto',
-  gap: '3rem',
-  width: '82%',  
-})
-
-export const SkillsContainer = styled('div', {
-  margin: '0 auto',
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-})
-
-export const SkillsContent = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  fontSize: '40px',
-  width: '100%',
-})
-
-export const SkillList = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-  gap: '2rem',
-  flexWrap: 'wrap',
-  marginTop: '2rem',
-})
-
-export const SkillStatus = styled('div',{
-  fontSize: '1rem'
-})
-export const SkillImgs = styled('div',{
-  display: 'flex',
-  justifyContent: 'space-between',
-  gap: '2rem',
-  flexWrap: 'wrap',
-})
-
-export const ProjectContent = styled('div', {
-  display:' flex',
-  flexDirection: 'row',
+export const HomeContainer = styled("main", {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
   alignItems: "center",
-  gap: '1rem',
-  justifyContent: 'center', 
-  flexWrap: 'wrap', 
-  minHeight: 656,
-  width: '100%',
-  marginLeft: 'auto',
-})
+  margin: "0 auto",
+  gap: "3rem",
+  width: "100%",
+  maxWidth: 1180,
+});
 
-export const Project = styled('a', {
-  display: 'flex',
-  flexDirection: 'column',
-  border: '1px solid #7289DA',
+export const SkillsContainer = styled("div", {
+  margin: "0 auto",
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+});
+
+export const SkillsContent = styled("div", {
+  margin: "0 auto",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+
+  fontSize: "40px",
+  width: "90%",
+});
+
+export const SkillList = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  gap: "2rem",
+  flexWrap: "wrap",
+  marginTop: "2rem",
+  width: "100%",
+});
+
+export const SkillStatus = styled("div", {
+  fontSize: "1rem",
+  color: "$subText",
+});
+export const SkillImgs = styled("div", {
+  display: "flex",
+  justifyContent: "space-between",
+  gap: "2rem",
+  flexWrap: "wrap",
+  padding: "0 1rem",
+});
+
+export const ProjectsContainer = styled("div", {
+  width: "100%",
+  maxWidth: 1180,
+  margin: "0 auto",
+  display: "flex",
+  flexDirection: "column",
+});
+
+export const ProjectContent = styled("div", {
+  width: "90%",
+  margin: "0 auto",
+  minHeight: 656,
+  span: {
+    width: "100%",
+    fontSize: "40px",
+    textAlign: "left",
+  },
+});
+
+export const ProjectsList = styled("div", {
+  width: "100%",
+  display: " flex",
+  flexDirection: "column",
+  alignItems: "center",
+  flexWrap: "wrap",
+  padding: "0 1rem",
+});
+
+export const Project = styled("div", {
+  width: "100%",
+  height: 180,
+  display: "flex",
   borderRadius: 8,
-  cursor: 'pointer',
-  padding: '0.25rem',
-  maxWidth: 600,
+  cursor: "pointer",
+  padding: "0.25rem",
+  marginTop: "2rem",
 
   img: {
-    objectFit: 'cover',
+    objectFit: "cover",
     borderRadius: 8,
   },
+});
 
-  footer: {
-    display: ' flex',
-    flexDirection: 'column',
-    padding: '0.25rem',
-    
-    strong: {
-      fontSize: '1.5rem',
-    },
-    
-    span: {
-      fontSize: "1rem",
-      color: '$subText'
-    }
+export const ProjectDescription = styled("div", {
+  display: " flex",
+  flexDirection: "column",
+  padding: "0.25rem",
+  width: "40%",
+  margin: "0.5rem 0 0 1rem",
 
-  }
-})
+  strong: {
+    fontSize: "1.5rem",
+  },
+
+  span: {
+    fontSize: "1rem",
+    color: "$subText",
+    marginTop: "1rem",
+    // whiteSpace: "nowrap",
+    overflow: "auto",
+    // textOverflow: "ellipsis",
+  },
+});
+
+export const ProjectLink = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  marginTop: "1rem",
+});
